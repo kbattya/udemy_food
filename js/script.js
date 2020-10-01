c15c386bda5e0f53a05fed229e4e066aa547b3a0
Kate <kbattya@ukr.net>
2020-10-01 11:09:13 +0300

    added timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+  // TABS
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -34,5 +36,62 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  // TIMER
+
+  const deadline = '2020-11-02';
+
+  function getTimeRemainig (endtime) {
+   const t = Date.parse(endtime) - Date.parse(new Date()),
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),
+         hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+         minutes = Math.floor((t / (1000 * 60)) % 60),
+         seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  
+
+  function setClock (selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = document.getElementById('days'),
+          hours = document.getElementById('hours'),
+          minutes = document.getElementById('minutes'),
+          seconds = document.getElementById('seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+  
+    updateClock();
+
+    function updateClock () {
+      const t = getTimeRemainig(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  
+  setClock('.timer', deadline);
+
 });
 
